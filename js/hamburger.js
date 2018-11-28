b14de617d2a9b79ad41ba9a3957909f028559078
Celentia <32060778+Celentia@users.noreply.github.com>
2018-11-28 16:55:01 +0200

    Delete hamburger.js

--- js/hamburger.js
+++
@@ -1,9 +0,0 @@
-$('.menu_icon').click (function(){
-  	$(this).toggleClass('open');
-  	$('.logo').toggleClass('open');
-  	$('.header__nav').toggleClass("open_menu");
-});
-$('.header__nav-item').click (function(){
-	$('.header__nav').toggleClass("open_menu");
-	$('.logo').toggleClass('open');
-})
